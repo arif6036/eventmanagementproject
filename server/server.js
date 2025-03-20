1b9caf3c0186fdc0a086973f10b0d584a3648a3c
arif6036 <arifminasrifa@gmail.com>
2025-03-20 21:18:38 +0530

    core updatted

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ connectDB();
 const app = express();
 
 // ✅ CORS Middleware (Dynamically from .env)
-const allowedOrigins = process.env.FRONTEND_URL || "http://localhost:5173" || ;
+const allowedOrigins = process.env.FRONTEND_URL || "http://localhost:5173" || "https://eventmanagementproject.vercel.app/";
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 // ✅ Middleware
