2e6b85e06669a422cf4db14cfa3f1a4e5a9b6a86
arif6036 <arifminasrifa@gmail.com>
2025-03-21 15:00:52 +0530

    updated vercel and request updated2

--- server/server.js
+++ server/server.js
@@ -54,4 +54,4 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-module.exports = app; // For Vercel
+// module.exports = app; // For Vercel
